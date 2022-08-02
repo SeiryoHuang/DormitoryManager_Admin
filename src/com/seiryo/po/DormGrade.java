c26ee4ab0ccfa70ce5ce5bfe2c6e1eaad3fec386
AminSeiryo <110449092+AminSeiryo@users.noreply.github.com>
2022-08-02 15:39:56 +0900

    Add files via upload

---
+++ src/com/seiryo/po/DormGrade.java
@@ -0,0 +1,18 @@
+package com.seiryo.po;
+
+import java.util.Date;
+
+public class DormGrade {
+
+    private Integer g_id;
+
+    private Integer d_id;
+
+    private String d_dormbuilding;
+
+    private Integer d_grade;
+
+    private Date create_time;
+
+    private Date update_time;
+}
